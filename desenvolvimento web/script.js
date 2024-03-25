ec350e4174c296bce940476b976c552eb5508834
tiago <75739193+p3rry-123@users.noreply.github.com>
2024-03-25 11:06:57 -0300

    Add files via upload

---
+++ desenvolvimento web/script.js
@@ -0,0 +1,15 @@
+let lista = [1,2,3,4]
+
+function minhafunc(){
+    return 'não faz nada'
+}
+
+console.log('no meio');
+
+lista.push('novo');
+
+console.log(lista.length);
+
+console.log(minhafunc('ou tudo.'));
+
+console.log('termionou');
